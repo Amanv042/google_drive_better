b80f5442d3c83937cf49a284d30b4ce30e69b979
Aman Verma <46923418+Amanv042@users.noreply.github.com>
2025-02-06 17:27:07 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
